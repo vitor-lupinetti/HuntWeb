44db7fb901774b830957c4cc3eb36efc742acd3e
vitor <vi.lupinetti@hotmail.com.br>
2020-04-27 16:45:29 -0300

    Finished

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Main from './pages/main';
+import Product from './pages/products';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path = "/" component = {Main}></Route>
+            <Route path = "/products/:id" component = {Product}></Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
